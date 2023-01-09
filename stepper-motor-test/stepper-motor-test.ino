42529fa0cd0fb69a96c7284e97da4c187d8f82f1
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-01-09 22:36:03 +0100

    playing with buttons and steppers

--- stepper-motor-test/stepper-motor-test.ino
+++ stepper-motor-test/stepper-motor-test.ino
@@ -1,15 +1,31 @@
 #include <Stepper.h>
 
 #define STEP_NUMBER 18
+const int BUTTON_PIN = 7;
 
-Stepper stepper_main(STEP_NUMBER, 8, 9, 10, 11);
+int lastState = HIGH;
+int currentState;
+
+int stepperSpeed = 60;
+int stepsPerRevolution = 2048;
 
 void setup() {
-stepper_main.setSpeed(60);
-stepper_main.step(2048);
-
+  Serial.begin(115200);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
 
+  currentState = digitalRead(BUTTON_PIN);
+  // Stepper stepper_main(STEP_NUMBER, 8, 9, 10, 11);
+  // stepper_main.setSpeed(stepperSpeed);
+  // stepper_main.step(stepsPerRevolution);
+
+  if (lastState == LOW && currentState == HIGH) {
+    Serial.println("The state changed from LOW to HIGH.");
+    // stepsPerRevolution = stepsPerRevolution * -1;
+    // stepper_main.step(stepsPerRevolution);
+  }
+
+  lastState = currentState;
 }
