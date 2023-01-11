ba93d58c2baffabd6be98bcfdca20d7749b2587c
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-01-11 22:04:44 +0100

    figuring out button states

--- stepper-motor-test/stepper-motor-test.ino
+++ stepper-motor-test/stepper-motor-test.ino
@@ -7,7 +7,7 @@ int lastState = HIGH;
 int currentState;
 
 int stepperSpeed = 60;
-int stepsPerRevolution = 2048;
+int stepsPerRevolution = 120;
 
 void setup() {
   Serial.begin(115200);
@@ -16,17 +16,25 @@ void setup() {
 
 void loop() {
 
-  currentState = digitalRead(BUTTON_PIN);
   Stepper stepper_main(STEP_NUMBER, 8, 9, 10, 11);
+  currentState = digitalRead(BUTTON_PIN);
+
   // stepper_main.setSpeed(stepperSpeed);
   // stepper_main.step(stepsPerRevolution);
 
   if (lastState == LOW && currentState == HIGH) {
     Serial.println("The state changed from LOW to HIGH.");
+    stepsPerRevolution = stepsPerRevolution;
+    stepper_main.setSpeed(stepperSpeed);
+    stepper_main.step(stepsPerRevolution);
+  }
+lastState = currentState;
+
+  if (lastState == LOW && currentState == HIGH) {
+    Serial.println("The state changed from LOW to HIGH again.");
     stepsPerRevolution = stepsPerRevolution * -1;
     stepper_main.setSpeed(stepperSpeed);
     stepper_main.step(stepsPerRevolution);
   }
-
   lastState = currentState;
 }
