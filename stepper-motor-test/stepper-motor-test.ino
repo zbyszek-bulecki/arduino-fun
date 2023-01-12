334c4f5bfcd3c23ec47804ac3d35cda8ad23a004
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-01-12 22:59:26 +0100

    finally made RL buttons work

--- stepper-motor-test/stepper-motor-test.ino
+++ stepper-motor-test/stepper-motor-test.ino
@@ -1,40 +1,40 @@
 #include <Stepper.h>
 
 #define STEP_NUMBER 18
-const int BUTTON_PIN = 7;
+const int RIGHT_BUTTON_PIN = 7;
+const int LEFT_BUTTON_PIN = 6;
 
-int lastState = HIGH;
-int currentState;
+int RightButtonLastState = HIGH;
+int LeftButtonLastState = HIGH;
+int RightButtonCurrentState;
+int LeftButtonCurrentState;
 
-int stepperSpeed = 60;
-int stepsPerRevolution = 120;
+int stepperSpeed = 120;
+int stepsPerRevolutionRight = 240;
+int stepsPerRevolutionLeft = -240;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
-
   Stepper stepper_main(STEP_NUMBER, 8, 9, 10, 11);
-  currentState = digitalRead(BUTTON_PIN);
+  stepper_main.setSpeed(stepperSpeed);
 
-  // stepper_main.setSpeed(stepperSpeed);
-  // stepper_main.step(stepsPerRevolution);
-
-  if (lastState == LOW && currentState == HIGH) {
-    Serial.println("The state changed from LOW to HIGH.");
-    stepsPerRevolution = stepsPerRevolution;
-    stepper_main.setSpeed(stepperSpeed);
-    stepper_main.step(stepsPerRevolution);
+  RightButtonCurrentState = digitalRead(RIGHT_BUTTON_PIN);
+  if (RightButtonLastState == LOW && RightButtonCurrentState == HIGH) {
+    Serial.println("Let's roll RIGHT!");
+    stepper_main.step(stepsPerRevolutionRight);
+  } else {
+    LeftButtonCurrentState = digitalRead(LEFT_BUTTON_PIN);
+    if (LeftButtonLastState == LOW && LeftButtonCurrentState == HIGH){
+      Serial.println("Let's roll LEFT!");
+      stepper_main.setSpeed(stepperSpeed);
+      stepper_main.step(stepsPerRevolutionLeft);
+    }
   }
-lastState = currentState;
-
-  if (lastState == LOW && currentState == HIGH) {
-    Serial.println("The state changed from LOW to HIGH again.");
-    stepsPerRevolution = stepsPerRevolution * -1;
-    stepper_main.setSpeed(stepperSpeed);
-    stepper_main.step(stepsPerRevolution);
-  }
-  lastState = currentState;
+  LeftButtonLastState = LeftButtonCurrentState;
+  RightButtonLastState = RightButtonCurrentState;
 }
