bd200b48e0d430e6786d7682225ea83f32b5cdc6
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-01-13 22:56:23 +0100

    now I'm having fun with pointers :P

--- stepper-motor-test/stepper-motor-test.ino
+++ stepper-motor-test/stepper-motor-test.ino
@@ -3,20 +3,26 @@
 #define STEP_NUMBER 18
 const int RIGHT_BUTTON_PIN = 7;
 const int LEFT_BUTTON_PIN = 6;
+const int RESET_BUTTON_PIN = 5;
 
 int RightButtonLastState = HIGH;
 int LeftButtonLastState = HIGH;
+int ResetButtonLastState = HIGH;
+
 int RightButtonCurrentState;
 int LeftButtonCurrentState;
+int ResetButtonCurrentState;
 
 int stepperSpeed = 120;
 int stepsPerRevolutionRight = 240;
 int stepsPerRevolutionLeft = -240;
+int motorPosition = 0;
 
 void setup() {
   Serial.begin(115200);
   pinMode(RIGHT_BUTTON_PIN, INPUT_PULLUP);
   pinMode(LEFT_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
 }
 
 void loop() {
@@ -27,14 +33,22 @@ void loop() {
   if (RightButtonLastState == LOW && RightButtonCurrentState == HIGH) {
     Serial.println("Let's roll RIGHT!");
     stepper_main.step(stepsPerRevolutionRight);
-  } else {
-    LeftButtonCurrentState = digitalRead(LEFT_BUTTON_PIN);
-    if (LeftButtonLastState == LOW && LeftButtonCurrentState == HIGH){
-      Serial.println("Let's roll LEFT!");
-      stepper_main.setSpeed(stepperSpeed);
-      stepper_main.step(stepsPerRevolutionLeft);
-    }
+    motorPosition = motorPosition + stepsPerRevolutionRight;
+    Serial.println(motorPosition, "This is the motor position: %s");
+  }
+  LeftButtonCurrentState = digitalRead(LEFT_BUTTON_PIN);
+  if (LeftButtonLastState == LOW && LeftButtonCurrentState == HIGH) {
+    Serial.println("Let's roll LEFT!");
+    stepper_main.step(stepsPerRevolutionLeft);
+    motorPosition = motorPosition + stepsPerRevolutionLeft;
+    Serial.println(motorPosition, "This is the motor position: %s");
+  }
+  ResetButtonCurrentState = digitalRead(RESET_BUTTON_PIN);
+  if (ResetButtonLastState == LOW && ResetButtonCurrentState == HIGH) {
+    Serial.println("It's RESET time!");
+    stepper_main.step(motorPosition);
   }
   LeftButtonLastState = LeftButtonCurrentState;
   RightButtonLastState = RightButtonCurrentState;
+  ResetButtonCurrentState = ResetButtonLastState;
 }
