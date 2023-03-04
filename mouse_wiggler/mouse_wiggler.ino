bdc625e2ee392234396fa55fc6f59bbbad02bd2e
zbyszek-bulecki <zbyszek.bulecki@gmail.com>
2023-03-04 22:21:46 +0100

    Mouse Wiggler project added

---
+++ mouse_wiggler/mouse_wiggler.ino
@@ -0,0 +1,13 @@
+#include "WiFi.h"
+#include "Stepper.h"
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
